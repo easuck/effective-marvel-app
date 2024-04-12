414c83f9a3d0362ba9f82d9aaa05713f13e0c0ce
Егор Асонов <egorasonovvv@gmail.com>
2024-04-12 21:34:25 +0600

    [+] Burger button on low resolution

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,49 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import styles from "./styles.module.css"
-import {Link, Outlet} from "react-router-dom";
+import {Link} from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import {IconContext} from "react-icons";
 
 const Header: FC = () => {
+    const [matches, setMatches] =
+        useState<boolean>(window.matchMedia("(min-width: 550px)").matches);
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+    useEffect(() => {
+        window
+            .matchMedia("(min-width: 550px)")
+            .addEventListener("change", e => setMatches(e.matches));
+    }, []);
+
+    useEffect(() => {
+
+    }, [isMenuOpen]);
+
     return (
         <>
             <header className={styles.header}>
                 <img className={styles.logo} src="/marvel_logo.svg" alt="logo"/>
-                <nav className={styles.nav}>
-                    <Link to="characters">Characters</Link>
-                    <Link to="comics">Comics</Link>
-                    <Link to="favourites">Favourites</Link>
-                </nav>
+                {
+                    matches ? (
+                        <nav className={styles.nav}>
+                            <Link to="characters">Characters</Link>
+                            <Link to="comics">Comics</Link>
+                            <Link to="favourites">Favourites</Link>
+                        </nav>
+                    ) : (
+                        <IconContext.Provider value={{size: "40px", color: "orange"}}>
+                            <GiHamburgerMenu onClick={() => setIsMenuOpen(!isMenuOpen)}/>
+                        </IconContext.Provider>
+                    )
+                }
+                {
+                    isMenuOpen &&
+                    <nav className={styles.nav}>
+                        <Link to="characters">Characters</Link>
+                        <Link to="comics">Comics</Link>
+                        <Link to="favourites">Favourites</Link>
+                    </nav>
+                }
             </header>
         </>
     );
