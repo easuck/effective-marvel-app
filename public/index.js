c7b36834988258b4446934dd62c3dae8b1e88fa2
Егор Асонов <egorasonovvv@gmail.com>
2024-05-12 17:17:48 +0600

    [+] Script for service worker registration

---
+++ public/index.js
@@ -0,0 +1,30 @@
+const publicVapidKey = "BNmO7o29hUpkX3h_0vE36sVXgtSWMYc6ydzC2oKp1CSV_DQWI8u_cp9tOKpA-36NCQo_lj5E9XNJgobGNcML57I";
+
+if ('serviceWorker' in navigator){
+    send().catch(err => console.error(err));
+}
+
+async function send(){
+    console.log("Registering service worker...");
+    const register = await navigator.serviceWorker.register("./sw.js", {
+        scope: "/",
+    });
+    console.log("Service Worker Registered...");
+
+    console.log("Registering Push...");
+    const subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: publicVapidKey,
+    });
+    console.log("Push Registered...");
+
+    console.log("Sending Push...");
+    await fetch("http://localhost:8080/subscribe", {
+        method: "POST",
+        body: JSON.stringify(subscription),
+        headers: {
+            "content-type": "application/json",
+        },
+    });
+    console.log("Push Sent...");
+}
