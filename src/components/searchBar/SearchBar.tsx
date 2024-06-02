1a8c97d721f2d398da29b0b20adf985f89a91856
Егор Асонов <egorasonovvv@gmail.com>
2024-06-03 03:32:42 +0600

    [+] Translated all page elements

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,20 +1,23 @@
 import React, {FC} from "react";
 import styles from "./styles.module.css";
+import {useTranslation} from "react-i18next";
 
 const SearchBar: FC<{subject: string, amount: number,
     inputHandler: (event: React.ChangeEvent<HTMLInputElement>) => void,
     callback: (event: React.MouseEvent<HTMLButtonElement>) => void,
     searchWord: string, canselDebounce: () => void}> =
     ({subject, amount, inputHandler, callback, searchWord, canselDebounce}) => {
+    const {t} = useTranslation();
+
     return (
         <section>
             <div className={styles.labelWrapper}>
-                <h3 className={styles.labelCharacters}>{subject}</h3>
+                <h3 className={styles.labelCharacters}>{t(subject)}</h3>
                 <h3 className={styles.labelAmount}>({amount})</h3>
             </div>
             <form className={styles.form}>
-                <input value={searchWord} placeholder={"Search for " + subject + " by Name"} onChange={(event) => inputHandler(event)}/>
-                <button onClick={(event) => {callback(event); canselDebounce()}}>SEARCH</button>
+                <input value={searchWord} placeholder={t("Search for " + subject + " by Name")} onChange={(event) => inputHandler(event)}/>
+                <button onClick={(event) => {callback(event); canselDebounce()}}>{t("SEARCH")}</button>
             </form>
         </section>
     )
