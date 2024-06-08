b3b82b3b410b4b379aaa58dc43326bd4d801b399
Егор Асонов <egorasonovvv@gmail.com>
2024-06-08 22:29:19 +0600

    [*] Small type fix

--- src/components/selector/Selector.tsx
+++ src/components/selector/Selector.tsx
@@ -1,6 +1,6 @@
-import {FC} from 'react';
+import {ChangeEvent, FC} from 'react';
 
-const Selector: FC<{options: string[], callback: (e) => void, defaultLng?: string | null, defaultTheme?: string | null}> =
+const Selector: FC<{options: string[], callback: (e: ChangeEvent<HTMLSelectElement>) => void, defaultLng?: string | null, defaultTheme?: string | null}> =
     ({options, callback, defaultLng, defaultTheme}) => {
     return (
         <select onChange={(e) => callback(e)} defaultValue={defaultLng ?? defaultTheme}>
